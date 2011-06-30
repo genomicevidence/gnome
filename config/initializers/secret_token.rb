d857d0fc9c8a3d4e8a955636405c5ad926a09a1c
Joon Lee <seouri@gmail.com>
2011-06-30 14:52:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gknome::Application.config.secret_token = '5dd0efc6c43674b32f64ea11c808168f055f336243b4232479b0fe52dbe3f78d24d070bfac35044337a58a2f6c52d833fb6f342b45218dfdda6337b646d1be08'
